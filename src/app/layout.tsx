a72a4dce92bbd111f7397c725353d8fa5378533b
Lalit sharma <geekymonkcp742k@gmail.com>
2023-07-03 00:33:18 +0530

    full fledged authentiation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,40 @@
-import '@/styles/globals.css'
+import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/toaster";
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
 
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+  title: "Campusbuddy",
+  description:
+    "CampusBuddy is a dynamic and vibrant online platform that serves as a hub for students to connect collaborate, and thrive within their campus community and across campuses ",
+};
+
+//importing custom fonts
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <Navbar />
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          {children}
+        </div>
+
+        <Toaster />
+      </body>
     </html>
-  )
+  );
 }
---
+++ src/components/SignIn.tsx
@@ -0,0 +1,38 @@
+import { FC } from "react";
+import { Icons } from "./Icons";
+import Link from "next/link";
+import UserAuthForm from "./UserAuthForm";
+
+const SignIn: FC = ({}) => {
+  return (
+    <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px] ">
+      <div className="flex flex-col space-y-2 text-center">
+        <Icons.logo className="mx-auto h-6 w-6" />
+        <h1 className="text-2xl font-semibold tracking-tight">Welcome back!</h1>
+
+        <p className="text-sm max-w-xs mx-auto">
+          By signing in to CampusBuddy, you agree to the User Agreement and
+          Sign-in Policy, ensuring account security, responsible use, and data
+          protection. Your continued use signifies your understanding and
+          agreement with these policies.
+        </p>
+
+        {/* SignIn form */}
+
+        <UserAuthForm />
+
+        <p className="px-8 text-center text-sm text-muted-foreground">
+          New to CampusBuddy?{" "}
+          <Link
+            href="/sign-up"
+            className="hover:text-brand text-sm underline underline-offset-4"
+          >
+            Sign Up
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
