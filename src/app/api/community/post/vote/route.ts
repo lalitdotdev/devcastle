2363655ec14de314b423e4dcdf8a616e7495530e
Lalit sharma <geekymonkcp742k@gmail.com>
2023-07-09 17:44:04 +0530

    api route for handling votes on Post and deleting /updating existingVote in db based on action

---
+++ src/app/api/community/post/vote/route.ts
@@ -0,0 +1,95 @@
+// Handling the logic for api route /community/post/vote
+
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { redis } from "@/lib/redis";
+import { PostVoteValidator } from "@/lib/validators/vote";
+import { CachedPost } from "@/types/redis";
+
+const CACHE_AFTER_VOTES = 1;
+// This API route is going to encapsulate One Piece One Central piece of the very very cool caching data logic that we're going to make use of in this application where we cache the most popular posts depending on UPVOTE and then we're able to fetch that data super quickly on the front end whenever somebody visits that post
+
+export async function PATCH(req: Request) {
+  // We're going to be using the zod library to validate the data that's coming in from the front end
+
+  try {
+    const body = req.json();
+    const { postId, voteType } = PostVoteValidator.parse(body);
+
+    const session = await getAuthSession();
+
+    if (!session?.user) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    // checking for existing vote
+    // if there is an existing vote, we're going to update it
+    // if there is no existing vote, we're going to create it
+
+    const existingVote = await db.vote.findFirst({
+      where: {
+        userId: session.user.id,
+        postId,
+      },
+    });
+
+    const post = await db.post.findUnique({
+      where: {
+        id: postId,
+      },
+      include: {
+        author: true,
+        votes: true,
+      },
+    });
+
+    if (!post) {
+      return new Response("Post not found", { status: 404 });
+    }
+
+    // if there is an existing vote, we're going to update it
+
+    if (existingVote) {
+      // if the vote type is the same as the existing vote type, we're going to delete the vote
+
+      if (existingVote.type === voteType) {
+        await db.vote.delete({
+          where: {
+            userId_postId: {
+              postId,
+              userId: session.user.id,
+            },
+          },
+        });
+        return new Response("Ok!");
+      }
+
+      // if the vote type is different from the existing vote type, we're going to update the current vote
+
+      await db.vote.update({
+        where: {
+          userId_postId: {
+            postId,
+            userId: session.user.id,
+          },
+        },
+        // data we're going to update is  the vote type
+        data: {
+          type: voteType,
+        },
+      });
+
+      // recount the votes
+
+      const votesAmt = post.votes.reduce((acc, vote) => {
+        if (vote.type === "UPVOTE") return acc + 1;
+
+        if (vote.type === "DOWNVOTE") return acc - 1;
+
+        return acc;
+      }, 0);
+
+      return new Response("OK!");
+    }
+  } catch (error) {}
+}
