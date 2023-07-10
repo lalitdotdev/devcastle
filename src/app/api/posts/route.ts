6835c7de7ad74f8b87a37dfd4b67955cb8034114
Lalit sharma <geekymonkcp742k@gmail.com>
2023-07-10 08:17:20 +0530

    Implement post fetching based on criteria, include related data, add zod schema validation with error handling

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -20,4 +20,55 @@ export async function GET(req: Request) {
 
     followedCommunitiesIds = followedCommunities.map((sub) => sub.community.id);
   }
+  try {
+    // inline zod schema for validation
+    const { limit, page, communityName } = z
+      .object({
+        limit: z.string(),
+        page: z.string(),
+        communityName: z.string().nullish().optional(),
+      })
+      .parse({
+        communityName: url.searchParams.get("communityName"),
+        limit: url.searchParams.get("limit"),
+        page: url.searchParams.get("page"),
+      });
+
+    let whereClause = {};
+
+    if (communityName) {
+      whereClause = {
+        community: {
+          name: communityName,
+        },
+      };
+    } else if (session) {
+      whereClause = {
+        community: {
+          id: {
+            in: followedCommunitiesIds,
+          },
+        },
+      };
+    }
+    // post fetching
+    const posts = await db.post.findMany({
+      take: parseInt(limit),
+      skip: (parseInt(page) - 1) * parseInt(limit), // skip should start from 0 for page 1
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: {
+        community: true,
+        votes: true,
+        author: true,
+        comments: true,
+      },
+      where: whereClause,
+    });
+
+    return new Response(JSON.stringify(posts));
+  } catch (error) {
+    return new Response("Could not fetch posts", { status: 500 });
+  }
 }
