49d6d241b27ed87ab0b5b02ceaebb86e11aed69e
Lalit sharma <geekymonkcp742k@gmail.com>
2023-07-16 00:43:49 +0530

    server route for comment creation in db and error handling

---
+++ src/app/api/community/post/comment/route.ts
@@ -0,0 +1,37 @@
+import { getAuthSession } from '@/lib/auth';
+import { db } from '@/lib/db';
+import { CommentValidator } from '@/lib/validators/comment';
+import { z } from 'zod';
+
+export async function PATCH(req: Request) {
+  try {
+    const body = await req.json();
+
+    const { postId, text, replyToId } = CommentValidator.parse(body);
+
+    const session = await getAuthSession();
+
+    if (!session?.user) {
+      return new Response('Unauthorised', { status: 401 });
+    }
+
+    // store comment in database
+    await db.comment.create({
+      data: {
+        text,
+        replyToId,
+        postId,
+        authorId: session.user.id,
+      },
+    });
+
+    return new Response('OK! Comment created in DB');
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response('Invalid POST request data passed', { status: 422 });
+    }
+    return new Response('Could not create comment , please try again later!', {
+      status: 500,
+    });
+  }
+}
