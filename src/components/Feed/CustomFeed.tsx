38a4be31d59146c4235fb279c12e6d2297f3c3c6
Lalit sharma <geekymonkcp742k@gmail.com>
2023-07-15 07:48:58 +0530

    customfeed for logged in user that shows posts from the followed communities only

---
+++ src/components/Feed/CustomFeed.tsx
@@ -0,0 +1,44 @@
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
+import { db } from "@/lib/db";
+import PostFeed from "../Posts/PostFeed";
+import { getAuthSession } from "@/lib/auth";
+
+const CustomFeed = async () => {
+  const session = await getAuthSession();
+
+  const followedCommunities = await db.subscription.findMany({
+    where: {
+      userId: session?.user?.id,
+    },
+    include: {
+      community: true,
+    },
+  });
+
+  const posts = await db.post.findMany({
+    where: {
+      community: {
+        name: {
+          in: followedCommunities.map(
+            (followedCommunity) => followedCommunity.community.id
+          ),
+        },
+      },
+    },
+    
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      votes: true,
+      author: true,
+      comments: true,
+      community: true,
+    },
+    take: INFINITE_SCROLLING_PAGINATION_RESULTS,
+  });
+
+  return <PostFeed initialPosts={posts} />;
+};
+
+export default CustomFeed;
