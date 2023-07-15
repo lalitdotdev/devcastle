a100f7ab3485623c6b1c7ceef192f9c74f1292ff
Lalit sharma <geekymonkcp742k@gmail.com>
2023-07-15 07:59:26 +0530

    implementation of conditional displaying of feed

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,19 +1,28 @@
-import { buttonVariants } from "@/components/ui/Button";
-import { HomeIcon } from "lucide-react";
-import Link from "next/link";
-import { FC } from "react";
+import CustomFeed from '@/components/Feed/CustomFeed';
+import GeneralFeed from '@/components/Feed/GeneralFeed';
+import { buttonVariants } from '@/components/ui/Button';
+import { getAuthSession } from '@/lib/auth';
+import { HomeIcon } from 'lucide-react';
+import Link from 'next/link';
 
-interface pageProps {}
+const page = async () => {
+  // TODO: {/* General feed ---> Logged Out
+  // TODO: Custom Feed -----> Logged In */}
 
-const page: FC<pageProps> = ({}) => {
+  const session = await getAuthSession();
+
   return (
     <>
       <h1 className="font-bold text-3xl md:text-4xl text-[#6366F1]">
         Your feed
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
-        {/* Feed */}
-        {/* community info */}
+        {/* Display either custom feed or general feed */}
+
+        {/* @ts-expect-error server component */}
+
+        {session ? <CustomFeed /> : <GeneralFeed />}
+
         <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
           <div className="bg-emerald-100 px-6 py-4">
             <p className="font-semibold py-3 flex items-center gap-1.5">
@@ -24,14 +33,14 @@ const page: FC<pageProps> = ({}) => {
           <dl className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
             <div className="flex justify-between gap-x-4 py-3">
               <p className="text-zinc-500">
-                Your personalised CampusBuddy feedpage. Come here to check in
+                Your personalized CampusBuddy feedPage. Come here to check in
                 with your favorite communities.
               </p>
             </div>
 
             <Link
               className={buttonVariants({
-                className: "w-full mt-4  mb-6",
+                className: 'w-full mt-4  mb-6',
               })}
               href="/cb/create"
             >
