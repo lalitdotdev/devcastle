21fc0694d59ea5b2e23ef5d77191d0da34746fa0
Lalit sharma <geekymonkcp742k@gmail.com>
2023-07-20 22:39:04 +0530

    small changes in auth pages

--- src/components/Auth/SignIn.tsx
+++ src/components/Auth/SignIn.tsx
@@ -1,13 +1,13 @@
 import { FC } from "react";
-import { Icons } from "../Icons";
+// import { Icons } from "../Icons";
 import Link from "next/link";
 import UserAuthForm from "./UserAuthForm";
 
 const SignIn: FC = ({}) => {
   return (
-    <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px] ">
-      <div className="flex flex-col space-y-2 text-center">
-        <Icons.logo className="mx-auto h-6 w-6" />
+    <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]bg-[#262a35] text-gray-400">
+      <div className="flex flex-col space-y-2 text-center ">
+        {/* <Icons.logo className="mx-auto h-6 w-6" /> */}
         <h1 className="text-2xl font-semibold tracking-tight">Welcome back!</h1>
 
         <p className="text-sm max-w-xs mx-auto">
--- src/components/Auth/SignUp.tsx
+++ src/components/Auth/SignUp.tsx
@@ -6,23 +6,22 @@ import { Icons } from "../Icons";
 
 const SignUp: FC = ({}) => {
   return (
-    <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px] ">
-      <div className="flex flex-col space-y-2 text-center">
-        <Icons.logo className="mx-auto h-6 w-6" />
-        <h1 className="text-2xl font-semibold tracking-tight">Sign Up</h1>
+    <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]bg-[#262a35] text-gray-400">
+      <div className="flex flex-col space-y-2 text-center ">
+        {/* <Icons.logo className="mx-auto h-6 w-6" /> */}
 
         <p className="text-sm max-w-xs mx-auto">
-          By signing up on campusBuddy, you agree to the User Agreement and
+          By signing up on our platform, you agree to the User Agreement and
           Sign-in Policy, ensuring account security, responsible use, and data
           protection.
         </p>
 
-        {/*  SignUp form */}
+        {/* SignIn form */}
 
         <UserAuthForm />
 
         <p className="px-8 text-center text-sm text-muted-foreground">
-          Already on CampusBuddy?{" "}
+          Already a curious guy?{" "}
           <Link
             href="/sign-in"
             className="hover:text-brand text-sm underline underline-offset-4"
