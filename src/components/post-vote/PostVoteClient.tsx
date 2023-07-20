4c6f0ff0466d97661e6d66b4de59dc3e6580eaac
Lalit sharma <geekymonkcp742k@gmail.com>
2023-07-20 22:20:11 +0530

    Small changes in post Vote Component

--- src/components/post-vote/PostVoteClient.tsx
+++ src/components/post-vote/PostVoteClient.tsx
@@ -1,16 +1,16 @@
-'use client';
+"use client";
 
-import { useCustomToast } from '@/hooks/use-custom-toast';
-import { cn } from '@/lib/utils';
-import { usePrevious } from '@mantine/hooks';
-import { VoteType } from '@prisma/client';
-import { ArrowBigDown, ArrowBigUp, Heart, HeartCrack } from 'lucide-react';
-import { FC, useEffect, useState } from 'react';
-import { Button } from '../ui/Button';
-import { useMutation } from '@tanstack/react-query';
-import { PostVoteRequest } from '@/lib/validators/vote';
-import axios, { AxiosError } from 'axios';
-import { toast } from '@/hooks/use-toast';
+import { useCustomToast } from "@/hooks/use-custom-toast";
+import { toast } from "@/hooks/use-toast";
+import { cn } from "@/lib/utils";
+import { PostVoteRequest } from "@/lib/validators/vote";
+import { usePrevious } from "@mantine/hooks";
+import { VoteType } from "@prisma/client";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import { ArrowBigDown, ArrowBigUp } from "lucide-react";
+import { FC, useEffect, useState } from "react";
+import { Button } from "../ui/Button";
 
 interface PostVoteClientProps {
   postId: string;
@@ -44,12 +44,12 @@ const PostVoteClient: FC<PostVoteClientProps> = ({
 
       // updating by patch request as we don't want to send the whole post object to the server just to update the votes field in the db request body
 
-      await axios.patch('/api/community/post/vote', payload);
+      await axios.patch("/api/community/post/vote", payload);
     },
 
     // error handling
     onError: (err: any, voteType) => {
-      if (voteType === 'UPVOTE') setVotesAmt(prev => prev - 1);
+      if (voteType === "UPVOTE") setVotesAmt(prev => prev - 1);
       else setVotesAmt(prev => prev + 1);
 
       // reset the current vote to previous vote
@@ -62,60 +62,59 @@ const PostVoteClient: FC<PostVoteClientProps> = ({
       }
 
       return toast({
-        title: 'Something went wrong!',
-        description: 'Your vote was not registered , Please try again later.',
-        variant: 'destructive',
+        title: "Something went wrong!",
+        description: "Your vote was not registered , Please try again later.",
+        variant: "destructive",
       });
     },
     onMutate: (type: VoteType) => {
       if (currentVote === type) {
         setCurrentVote(undefined);
-        if (type === 'UPVOTE') setVotesAmt(prev => prev - 1);
-        else if (type === 'DOWNVOTE') setVotesAmt(prev => prev + 1);
+        if (type === "UPVOTE") setVotesAmt(prev => prev - 1);
+        else if (type === "DOWNVOTE") setVotesAmt(prev => prev + 1);
       } else {
         setCurrentVote(type);
-        if (type === 'UPVOTE')
+        if (type === "UPVOTE")
           setVotesAmt(prev => prev + (currentVote ? 2 : 1));
-        else if (type === 'DOWNVOTE')
+        else if (type === "DOWNVOTE")
           setVotesAmt(prev => prev - (currentVote ? 2 : 1));
       }
     },
   });
 
   return (
-    <div className="flex flex-col gap-4 sm:gap-0 pr-6 sm:w-20 pb-4 sm:pb-0 ">
+    <div className="flex md:flex-col gap-0 pr-6 md:w-20 md:pb-4 sm:pb-0 items-center">
       {/* upvote */}
       <Button
-        onClick={() => vote('UPVOTE')}
+        onClick={() => vote("UPVOTE")}
         size="sm"
-        variant="ghost"
         aria-label="upvote"
+        className="bg-transparent"
       >
         <ArrowBigUp
-          className={cn('h-5 w-5 text-zinc-700', {
+          className={cn("h-5 w-5 text-gray-400", {
             // cn helper => conditionally apply the classNames
-            'text-emerald-500 fill-emerald-500': currentVote === 'UPVOTE',
+            "text-emerald-500 fill-emerald-500": currentVote === "UPVOTE",
           })}
         />
       </Button>
       {/* score */}
-      <p className="text-center py-2 font-medium text-sm text-zinc-900">
+      <p className="text-center py-2 font-medium text-md text-gray-400">
         {votesAmt}
       </p>
 
       {/* downvote */}
       <Button
-        onClick={() => vote('DOWNVOTE')}
+        onClick={() => vote("DOWNVOTE")}
         size="sm"
-        className={cn({
-          'text-emerald-500': currentVote === 'DOWNVOTE',
+        className={cn("bg-transparent", {
+          "text-emerald-500": currentVote === "DOWNVOTE",
         })}
-        variant="ghost"
         aria-label="downvote"
       >
         <ArrowBigDown
-          className={cn('h-5 w-5 text-zinc-700', {
-            'text-red-500 fill-red-500': currentVote === 'DOWNVOTE',
+          className={cn("h-5 w-5 text-gray-400", {
+            "text-red-500 fill-red-500": currentVote === "DOWNVOTE",
           })}
         />
       </Button>
