54f86e12d8e7b60eae40fd3cd314467c5dfe2c15
Lalit sharma <geekymonkcp742k@gmail.com>
2023-07-23 21:53:32 +0530

    Prevent Editor Output Overflow

--- src/components/renderers/CustomCodeRenderer.tsx
+++ src/components/renderers/CustomCodeRenderer.tsx
@@ -4,8 +4,10 @@ function CustomCodeRenderer({ data }: any) {
   data;
 
   return (
-    <pre className="bg-gray-800 rounded-md p-4">
-      <code className="text-gray-100 text-sm">{data.code}</code>
+    <pre className="bg-gray-700 rounded-md p-4 break-words ">
+      <code className="text-gray-100 text-sm whitespace-pre-wrap ">
+        {data.code}
+      </code>
     </pre>
   );
 }
