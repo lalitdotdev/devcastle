699ccef58b161aefa7a965fca653b3d8e38732a5
Lalit sharma <geekymonkcp742k@gmail.com>
2023-07-24 19:51:15 +0530

    layout style changes in UserAccountNav

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { User } from "next-auth";
+import { signOut } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { FC } from "react";
+import UserAvatar from "./UserAvatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,9 +13,6 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/DropdownMenu";
-import UserAvatar from "./UserAvatar";
-import Link from "next/link";
-import { signOut } from "next-auth/react";
 
 // import { signOut } from "next-auth/react";
 
@@ -24,15 +25,15 @@ const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
     <DropdownMenu>
       <DropdownMenuTrigger>
         <UserAvatar
-          className="h-8 w-8"
+          className="h-8 w-8 border-2 border-zinc-300 "
           user={{
             name: user.name || null,
             image: user.image || null,
           }}
         />
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="bg-white" align="end">
-        <div className="flex items-center justify-start gap-2 p-2">
+      <DropdownMenuContent className="bg-[#1B1F23] text-gray-400" align="end">
+        <div className="flex items-center justify-start gap-2 p-2 text-gray-400">
           <div className="flex flex-col space-y-1 leading-none">
             {user.name && <p className="font-medium">{user.name}</p>}
             {user.email && (
@@ -52,12 +53,12 @@ const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
         </DropdownMenuItem>
 
         <DropdownMenuItem asChild>
-          <Link href="/settings">Settings</Link>
+          <Link href="/profile">Profile</Link>
         </DropdownMenuItem>
         <DropdownMenuSeparator />
         <DropdownMenuItem
           className="cursor-pointer"
-          onSelect={(event) => {
+          onSelect={event => {
             event.preventDefault();
             signOut({
               callbackUrl: `${window.location.origin}/sign-in`,
