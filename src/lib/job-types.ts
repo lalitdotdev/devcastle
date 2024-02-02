4ea42eb04d64e062da3cec9c1b0f1708cb3dd7c6
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-03 00:26:35 +0530

    Chore(lib): Define jobTypes and locationTypes arrays for job and location classifications

---
+++ src/lib/job-types.ts
@@ -0,0 +1,19 @@
+export const jobTypes = [
+    "Full-time",
+    "Part-time",
+    "Contract",
+    "Temporary",
+    "Volunteer",
+    "Internship",
+    "Apprenticeship",
+    "Research"
+]
+
+export const locationTypes = [
+    "Remote",
+    "On-site",
+    "Hybrid",
+    "Relocation",
+    "Travel",
+    "Other",
+]
