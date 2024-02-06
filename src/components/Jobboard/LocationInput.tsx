0a91b303f0ff66a96c02ccf6fa33011863e9694c
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-06 16:42:43 +0530

    feat(location search): Implemented location search functionality in the LocationInput component using a list of cities to provide autocomplete suggestions based on user input, enhancing user experience and interaction with the input field.

--- src/components/Jobboard/LocationInput.tsx
+++ src/components/Jobboard/LocationInput.tsx
@@ -1,10 +1,43 @@
-import { forwardRef } from "react";
-
+import { forwardRef, useMemo, useState } from "react";
+import citiesList from "@/lib/cities-list";
+import { Input } from "../ui/Input";
 interface LocationInputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   onLocationSelected: (location: string) => void;
 }
 
 export default forwardRef<HTMLInputElement, LocationInputProps>(
-  function LocationInput({}, ref) {}
+  function LocationInput({ onLocationSelected, ...props }, ref) {
+    const [locationSearchInput, setLocationSearchInput] = useState("");
+    const [hasFocus, setHasFocus] = useState(false);
+
+    const cities = useMemo(() => {
+      if (!locationSearchInput.trim()) return [];
+
+      const searchWords = locationSearchInput.split(" ");
+
+      return citiesList
+        .map((city) => `${city.name}, ${city.subcountry}, ${city.country}`)
+        .filter(
+          (city) =>
+            city.toLowerCase().startsWith(searchWords[0].toLowerCase()) &&
+            searchWords.every((word) =>
+              city.toLowerCase().includes(word.toLowerCase())
+            )
+        )
+        .slice(0, 5);
+    }, [locationSearchInput]);
+
+    return (
+      <div>
+        <Input
+          value={locationSearchInput}
+          onChange={(e) => setLocationSearchInput(e.target.value)}
+          {...props}
+          ref={ref}
+        />
+        <div>{JSON.stringify(cities)}</div>
+      </div>
+    );
+  }
 );
