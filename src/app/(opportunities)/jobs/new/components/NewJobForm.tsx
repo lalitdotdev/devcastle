aef5069b418653a29b33226fe772df22f2d2ca44
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-06 18:23:58 +0530

    feat(form and error handling) 🚀 : Implemented NewJobForm component utilizing React Hook Form for efficient form management, with controlled input components for job details such as title, type, company name, company logo, location type, and office location, incorporating zodResolver for schema-based validation.

--- src/app/(opportunities)/jobs/new/components/NewJobForm.tsx
+++ src/app/(opportunities)/jobs/new/components/NewJobForm.tsx
@@ -1,9 +1,29 @@
+"use client";
+
+// import Select from "@/components/Jobboard/Select";
 import H1 from "@/components/h1";
-import { Form } from "@/components/ui/Form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/Form";
+import { Input } from "@/components/ui/Input";
+
+import { toast } from "@/hooks/use-toast";
+import { jobTypes } from "@/lib/job-types";
 import { CreateJobValues, createJobSchema } from "@/lib/validators/jobFilter";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FC } from "react";
 import { useForm } from "react-hook-form";
+import { locationTypes } from "../../../../../lib/job-types";
+
+import LocationInput from "@/components/Jobboard/LocationInput";
+import { X } from "lucide-react";
+import Select from "@/components/Jobboard/Select";
 
 interface NewJobFormProps {}
 
@@ -11,6 +31,27 @@ const NewJobForm: FC<NewJobFormProps> = ({}) => {
   const form = useForm<CreateJobValues>({
     resolver: zodResolver(createJobSchema),
   });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty, isSubmitting, isValid },
+    watch,
+    trigger,
+    control,
+    setValue,
+    setFocus,
+  } = form;
+
+  const onSubmit = async (data: CreateJobValues) => {
+    toast({
+      description: (
+        <pre>
+          <code>{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  };
 
   return (
     <main className="m-auto my-10 max-w-3xl space-y-10">
@@ -25,14 +66,176 @@ const NewJobForm: FC<NewJobFormProps> = ({}) => {
           help you find the best candidates.
         </p>
       </div>
-      <div className="space-y-6 rounded-lg border border-zinc-400 p-4">
+      <div className="space-y-6 rounded-lg border border-zinc-600 p-4">
         <div>
           <h2 className="font-semibold">Job details</h2>
           <p className="text-muted-foreground">
             Provide a job description and details
           </p>
         </div>
-        <Form {...form}></Form>
+        <Form {...form}>
+          <form
+            className="space-y-4"
+            noValidate
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <FormField
+              control={control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Job title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="e.g. Frontend" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    A descriptive title that will help developers find your job
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={control}
+              name="type"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Job type</FormLabel>
+                  <FormControl>
+                    <Select {...field} defaultValue="" className="bg-[#282C35]">
+                      <option value="" hidden>
+                        Select an option
+                      </option>
+                      {jobTypes.map((jobType) => (
+                        <option key={jobType} value={jobType}>
+                          {jobType}
+                        </option>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <FormDescription>
+                    Full-time, part-time, contract, etc.
+                  </FormDescription>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={control}
+              name="companyName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Company</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    The name of the company that is hiring for this position
+                    (e.g. Google)
+                  </FormDescription>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={control}
+              name="companyLogo"
+              render={({ field: { value, ...fieldValues } }) => (
+                <FormItem>
+                  <FormLabel>Company logo</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...fieldValues}
+                      type="file"
+                      accept="image/*"
+                      onChange={(e) => {
+                        const file = e.target.files?.[0];
+                        fieldValues.onChange(file);
+                      }}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    A logo that represents the company (optional)
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={control}
+              name="locationType"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Location</FormLabel>
+                  <FormControl>
+                    <Select
+                      {...field}
+                      defaultValue=""
+                      className="bg-[#282C35]"
+                      onChange={(e) => {
+                        field.onChange(e);
+                        if (e.currentTarget.value === "Remote") {
+                          trigger("location");
+                        }
+                      }}
+                    >
+                      <option value="" hidden>
+                        Select an option
+                      </option>
+                      {locationTypes.map((locationType) => (
+                        <option key={locationType} value={locationType}>
+                          {locationType}
+                        </option>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <FormDescription>
+                    Select the location type for this job (e.g. Remote, On-site)
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={control}
+              name="location"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Office location</FormLabel>
+                  <FormControl>
+                    <LocationInput
+                      onLocationSelected={field.onChange}
+                      ref={field.ref}
+                    />
+                  </FormControl>
+
+                  {watch("location") && (
+                    <div className="flex items-center gap-1">
+                      <button
+                        type="button"
+                        className="border-2 border-indigo-600 rounded-md p-1 hover:bg-indigo-600 hover:text-white transition-colors duration-200 ease-in-out"
+                        onClick={() => {
+                          setValue("location", "", { shouldValidate: true });
+                        }}
+                      >
+                        <X size={20} />
+                      </button>
+                      <span className="text-sm">{watch("location")}</span>
+                    </div>
+                  )}
+                  <FormDescription>
+                    The location of the job (optional)
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
       </div>
     </main>
   );
