84a003a7dfef9cc9d1f9d5f6578981c58138352f
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-06 14:04:20 +0530

    feat(job-validation): Implement Zod schema for creating a job with validation rules for required fields, job type, salary, and optional description.

--- src/lib/validators/jobFilter.ts
+++ src/lib/validators/jobFilter.ts
@@ -1,4 +1,8 @@
 import { z } from "zod";
+import { jobTypes } from "../job-types";
+
+const requiredString = z.string().min(1, "Required");
+const numericRequiredString = requiredString.regex(/^\d+$/, "Must be a number");
 
 const companyLogoSchema = z
   .custom<File | undefined>()
@@ -9,6 +13,20 @@ const companyLogoSchema = z
   .refine((file) => {
     return !file || file.size < 1024 * 1024 * 2;
   }, "File must be less than 2MB");
+
+export const createJobSchema = z.object({
+  title: requiredString.max(100),
+  type: requiredString.refine(
+    (value) => jobTypes.includes(value),
+    "Invalid job type"
+  ),
+  companyName: requiredString.max(100),
+  companyLogo: companyLogoSchema,
+  description: z.string().max(5000).optional(),
+  salary: numericRequiredString.max(9, "Number can't be longer than 9 digits"),
+});
+
+export type CreateJobValues = z.infer<typeof createJobSchema>;
 
 export const jobFilterSchema = z.object({
   q: z.string().optional(),
