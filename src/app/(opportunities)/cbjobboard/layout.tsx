9a2166ed3765fcf96e55c7e1013687d368bc5e82
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-06 10:43:26 +0530

    feat(footer): Implement Footer component with company information and navigation links on opportunities layout page.

--- src/app/(opportunities)/cbjobboard/layout.tsx
+++ src/app/(opportunities)/cbjobboard/layout.tsx
@@ -1,11 +1,14 @@
+import Footer from "@/components/Jobboard/Footer";
+
 export default function JobBoardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex text-white min-w-[350px] scrollbar-thin !scrollbar-track-transparent !scrollbar-thumb-[#212329]">
+    <div className="text-white min-w-[350px]">
       {children}
+      <Footer />
     </div>
   );
 }
