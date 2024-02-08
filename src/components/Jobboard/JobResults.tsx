9a25849175fcf2615aa940ff3c68a8dd71fbea0b
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-08 14:39:22 +0530

    refactor(jobresults) 🔎 : Enhance JobResults component to dynamically link to individual job details page navigation.

--- src/components/Jobboard/JobResults.tsx
+++ src/components/Jobboard/JobResults.tsx
@@ -2,6 +2,7 @@ import { Prisma } from "@prisma/client";
 import JobListItem from "./JobListItem";
 import { JobFilterValues } from "../../lib/validators/jobFilter";
 import { db } from "@/lib/db";
+import Link from "next/link";
 
 interface JobResultsProps {
   filterValues: JobFilterValues;
@@ -45,9 +46,10 @@ const JobResults = async ({
   return (
     <div className="grow space-y-4 overflow-hidden ">
       {jobs.map((job) => (
-        <JobListItem key={job.id} job={job} />
+        <Link href={`/jobs/${job.slug}`} key={job.id} className="block ">
+          <JobListItem job={job} />
+        </Link>
       ))}
-
       {jobs.length === 0 && (
         <div className="flex items-center justify-center h-64">
           <p className="text-gray-500 text-xl font-semibold">
