d4e1d6b3a94c62cf01b358357ca40d6ce2ffb574
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-08 13:44:37 +0530

    feat(next.config.js) ⚒ : Configure next.config.js to include experimental features and define allowed image domains for Next.js Image component.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['uploadthing.com', 'lh3.googleusercontent.com'],
+    domains: [
+      "uploadthing.com",
+      "lh3.googleusercontent.com",
+      "7whi4qsyghrar8gh.public.blob.vercel-storage.com",
+    ],
   },
   experimental: {
     appDir: true,
     serverActions: true,
-  }
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
