080ab943dd5f66366adc94be5a2f376ad421a993
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-11 15:14:32 +0530

    feat(ADMIN DASHBOARD) 🚀 : Create Layout component to serve as a common layout for the admin dashboard, setting default metadata title to 'Admin Dashboard'

---
+++ src/app/(admin)/admin/layout.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Admin Dashboard",
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="text-gray-400 min-h-screen overflow-auto">{children}</div>
+  );
+}
