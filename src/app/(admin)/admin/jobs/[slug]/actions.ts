aca8b60be0f773abc4b630f27c9a907fe669043f
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-11 15:21:17 +0530

    ⚒ feat(admin side server-actions): Implement deleteJob function to handle job deletion by admins, ensuring authorization based on user role, deleting associated company logo if exists, and triggering revalidation of the root path upon successful deletion.

--- src/app/(admin)/admin/jobs/[slug]/actions.ts
+++ src/app/(admin)/admin/jobs/[slug]/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
-
+import { del } from "@vercel/blob";
 import { revalidatePath } from "next/cache";
 
 type FormState = { error?: string } | undefined;
@@ -25,6 +25,37 @@ export async function approveSubmission(
     revalidatePath("/");
   } catch (error) {
     let message = "An error occurred while approving the submission";
+    if (error instanceof Error) {
+      message = error.message;
+    }
+    return { error: message };
+  }
+}
+
+// delete a job server action
+export async function deleteJob(
+  prevState: FormState,
+  formData: FormData
+): Promise<FormState> {
+  try {
+    // throw new Error("Not implemented");
+    const jobId = parseInt(formData.get("jobId") as string);
+    const session = await getAuthSession();
+    if (session?.user.role !== "ADMIN") {
+      throw new Error("Not Authorized !!");
+    }
+    const job = await db.job.findUnique({
+      where: { id: jobId },
+    });
+    if (job?.companyLogoUrl) {
+      await del(job.companyLogoUrl);
+    }
+    await db.job.delete({
+      where: { id: jobId },
+    });
+    revalidatePath("/");
+  } catch (error) {
+    let message = "An error occurred while deleting the job";
     if (error instanceof Error) {
       message = error.message;
     }
