f3e0dd2190d755e8a3e693f65fcad2ea6858c5ba
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-11 12:34:00 +0530

    feat(FormsubmitButton and server actions): Updated FormSubmitButton component to use useFormStatus hook instead of deprecated experimental_useFormStatus.

--- src/components/Jobboard/FormSubmitButton.tsx
+++ src/components/Jobboard/FormSubmitButton.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { experimental_useFormStatus } from "react-dom";
+import { useFormStatus } from "react-dom";
 import LoadingButton from "./LoadingButton";
 
 export default function FormSubmitButton(
   props: React.ButtonHTMLAttributes<HTMLButtonElement>
 ) {
-  const { pending } = experimental_useFormStatus();
+  const { pending } = useFormStatus();
   return <LoadingButton {...props} type="submit" loading={pending} />;
 }
