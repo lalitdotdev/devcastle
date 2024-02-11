4b5cd385cb56cdcdad3a43851cf0dd24b2e7ce34
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-11 14:53:29 +0530

    feat(providers) ⚙ : Implement Providers component to wrap children with QueryClientProvider for React Query, SessionProvider for NextAuth, and commented out NextThemesProvider with configurable theme attributes.

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,6 +2,8 @@
 
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
 import { FC, ReactNode } from "react";
 
 interface LayoutProps {
@@ -10,10 +12,20 @@ interface LayoutProps {
 
 const queryClient = new QueryClient();
 
-const Providers: FC<LayoutProps> = ({ children }) => {
+const Providers: FC<LayoutProps> = ({
+  children,
+  ...props
+}: ThemeProviderProps) => {
   return (
     <QueryClientProvider client={queryClient}>
+      {/* <NextThemesProvider
+        attribute="class"
+        defaultTheme="light"
+        enableSystem
+        {...props}
+      > */}
       <SessionProvider>{children}</SessionProvider>
+      {/* </NextThemesProvider> */}
     </QueryClientProvider>
   );
 };
