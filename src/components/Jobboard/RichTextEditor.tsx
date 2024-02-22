55fd882377a72638e59d75d845dde5078617369b
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-22 08:47:23 +0530

    fixes(type error): Argument of type '() => Promise<typeof Editor>' is not assignable to parameter of type 'DynamicOptions<{}> | Loader<{}>'.

--- src/components/Jobboard/RichTextEditor.tsx
+++ src/components/Jobboard/RichTextEditor.tsx
@@ -4,38 +4,40 @@ import { cn } from "@/lib/utils";
 import dynamic from "next/dynamic";
 import { forwardRef } from "react";
 import { EditorProps } from "react-draft-wysiwyg";
+
+// import { EditorProps } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
-const Editor = dynamic(
-  () => import("react-draft-wysiwyg").then((mod) => mod.Editor),
-  { ssr: false }
-);
 
-export default forwardRef<Object, EditorProps>(function RichTextEditor(
-  props,
-  ref
-) {
-  return (
-    <Editor
-      editorClassName={cn(
-        "border border-zinc-500 rounded-md px-3 min-h-[150px] focus-within:border-indigo-500 cursor-text ring-offset-background focus-within:outline-none focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2 bg-[#1B1F23] ",
-        props.editorClassName
-      )}
-      toolbar={{
-        options: ["inline", "list", "link", "history"],
-        inline: {
-          options: ["bold", "italic", "underline"],
-        },
-      }}
-      toolbarClassName={cn("", props.toolbarClassName)}
-      editorRef={(r) => {
-        if (typeof ref === "function") {
-          ref(r);
-        } else if (ref) {
-          ref.current = r;
-        }
-      }}
-      {...props}
-    />
-  );
-});
+const Editor = dynamic<{}>(
+    () => import("react-draft-wysiwyg").then((mod) => mod.Editor as any),
+    { ssr: false },
+) as any;
+
+
+export default forwardRef<Object, EditorProps>(
+    function RichTextEditor(props, ref) {
+        return (
+            <Editor
+                editorClassName={cn(
+                    "border rounded-md px-3 min-h-[150px] cursor-text ring-offset-background focus-within:outline-none focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2",
+                    props.editorClassName,
+                )}
+                toolbar={{
+                    options: ["inline", "list", "link", "history"],
+                    inline: {
+                        options: ["bold", "italic", "underline"],
+                    },
+                }}
+                editorRef={(r: Object | null) => {
+                    if (typeof ref === "function") {
+                        ref(r);
+                    } else if (ref) {
+                        ref.current = r;
+                    }
+                }}
+                {...props}
+            />
+        );
+    },
+);
