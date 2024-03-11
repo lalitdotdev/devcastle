88dde7ae39d23500407597a8cbe72fd032950e9c
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-11 13:58:39 +0530

    feat(post-skeleton): Implement detailed Skeleton component for loading state in PostSkeleton.

---
+++ src/components/Posts/Skeleton.tsx
@@ -0,0 +1,25 @@
+import Skeleton from "@/components/ui/skeleton";
+
+export function PostSkeleton() {
+    return (
+        <div className="p-3 rounded-xl bg-[#1F1F21]">
+            <div className="flex items-center justify-between mb-4">
+                <div className="flex items-center space-x-4">
+                    <Skeleton className="h-12 w-12 rounded-full" />
+                    <div>
+                        <Skeleton className="h-4 w-24" />
+                    </div>
+                </div>
+            </div>
+            <Skeleton className="h-6 w-3/4 mb-4" />
+            <Skeleton className="h-6 w-full mb-4" />
+            <Skeleton className="h-6 w-1/2 mb-4" />
+            <Skeleton className="h-6 w-3/4 mb-4" />
+            <div className="flex items-center justify-between">
+                <Skeleton className="h-6 w-20" />
+                <Skeleton className="h-6 w-20" />
+                <Skeleton className="h-6 w-20" />
+            </div>
+        </div>
+    );
+}
