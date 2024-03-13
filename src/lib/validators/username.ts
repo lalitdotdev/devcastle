c2048fc8fce4be9aa1346c6dadf4edbfbd956540
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-14 00:45:59 +0530

    Feat(Zod Schema Validators for dashboard form): Added Zod schema validation for user profile fields, including name and about, enforcing length and format constraints for data integrity and consistency.

--- src/lib/validators/username.ts
+++ src/lib/validators/username.ts
@@ -6,5 +6,12 @@ export const UserProfileValidator = z.object({
     .min(3)
     .max(32)
     .regex(/^[a-zA-Z0-9_]+$/),
-  about: z.string().max(500),
+  about: z
+    .string()
+    .min(10, {
+      message: "Bio must be at least 10 characters.",
+    })
+    .max(160, {
+      message: "Bio must not be longer than 30 characters.",
+    }),
 });
