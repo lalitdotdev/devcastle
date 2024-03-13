9005f37acc6ca7ce032742b3e2b3459ac11d0c43
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-14 00:52:31 +0530

    Feat(Markdown): Created a Markdown component using ReactMarkdown to render Markdown content with custom styling, including spacing, borders, and text colors, while also customizing the rendering of unordered lists and links with specific styles.

--- src/components/Jobboard/Markdown.tsx
+++ src/components/Jobboard/Markdown.tsx
@@ -1,21 +1,22 @@
 import ReactMarkdown from "react-markdown";
 
 interface MarkdownProps {
-  children: string;
+    children: string;
 }
 
 export default function Markdown({ children }: MarkdownProps) {
-  return (
-    <ReactMarkdown
-      className="space-y-3"
-      components={{
-        ul: (props) => <ul className="list-inside list-disc" {...props} />,
-        a: (props) => (
-          <a className="text-green-500 underline" target="_blank" {...props} />
-        ),
-      }}
-    >
-      {children}
-    </ReactMarkdown>
-  );
+    return (
+        <ReactMarkdown
+            className="space-y-3 p-2  md:p-8 border-2 border-gray-600 rounded-md text-neutral-300"
+            components={{
+                ul: (props) => <ul className="list-inside list-disc marker:text-blue-600 marker:text-lg " {...props} />,
+                a: (props) => (
+                    <a className="text-green-500 underline" target="_blank" {...props} />
+                ),
+
+            }}
+        >
+            {children}
+        </ReactMarkdown>
+    );
 }
