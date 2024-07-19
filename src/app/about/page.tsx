b17d15899096146708115e46bcffef4cc344d2a4
Lalit sharma <geekymonkcp742k@gmail.com>
2024-07-19 23:11:36 +0530

    🐛 fix(about page contact route): add portfolio link to contact

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,15 +19,16 @@ export const metadata = {
 };
 
 export default function About() {
+
     return (
         <>
-            <div className="mt-32 flex-col flex lg:grid lg:grid-cols-12 px-8">
-                <div className="text-7xl col-span-4 font-bold">
+            <div className="py-12 flex-col flex lg:grid lg:grid-cols-12 px-8 overflow-hidden">
+                <div className="text-5xl md:text-7xl col-span-4 font-bold">
                     <h1 className="mb-8 lg:pl-8 text-color-accentBlue">
                         About <span className="text-light-gray">Devcastle</span>
                     </h1>
                 </div>
-                <div className="font-mono col-start-6 col-span-5 max-w-xl lg:max-w-none text-lg md:text-2xl  leading-[130%] text-light-gray font-medium space-y-7">
+                <div className=" col-start-6 col-span-5 max-w-xl lg:max-w-none text-sm  md:text-2xl  leading-[130%] text-light-gray font-sm space-y-7 tracking-wide leading-5">
                     <p className="space-y-8 flex flex-col">
                         <span>
                             <strong>DevCastle</strong> is a platform by the developers and for the developers to connect talents with opportunities. We are a community of developers, designers, and creators who are passionate about building and creating things.
@@ -50,16 +51,16 @@ export default function About() {
                             />
                         </div> */}
                         <div className="text-primary text-base xl:text-h6 2xl:text-h5 font-medium flex flex-col">
-                            <p className=" leading-tight text-color-5 font-semibold ">Lalit Sharma</p>
+                            <p className="  text-color-5 font-semibold ">Lalit Sharma</p>
                             <Clipboard />
                         </div>
                     </div>
                 </div>
             </div>
 
-            <h2 className="mt-8 font-semibold text-lg md:text-2xl self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-n-14 dark:text-light dark:font-normal ">
+            <h2 className="mt-8 font-light text-sm md:text-2xl self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-n-14 dark:text-light dark:font-normal ">
                 Have a project in mind? Reach out to me 📞 from{' '}
-                <Link href="/contact" className="!underline underline-offset-2">
+                <Link href="https://litsharmadev.tech/" rel="noopener noreferrer" target="_blank" className="!underline underline-offset-2" >
                     here
                 </Link>{' '}
                 and let&apos;s make it happen.
