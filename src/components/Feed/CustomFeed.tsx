945722e6f2ef1d636ef10b66756846609f40df05
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-30 02:13:54 +0530

    🔖 Include bookmarks in custom feed query results.

--- src/components/Feed/CustomFeed.tsx
+++ src/components/Feed/CustomFeed.tsx
@@ -1,44 +1,45 @@
 import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
-import { db } from "@/lib/db";
 import PostFeed from "../Posts/PostFeed";
+import { db } from "@/lib/db";
 import { getAuthSession } from "@/lib/auth";
 
 const CustomFeed = async () => {
-  const session = await getAuthSession();
+    const session = await getAuthSession();
 
-  const followedCommunities = await db.subscription.findMany({
-    where: {
-      userId: session?.user?.id,
-    },
-    include: {
-      community: true,
-    },
-  });
-
-  const posts = await db.post.findMany({
-    where: {
-      community: {
-        name: {
-          in: followedCommunities.map(
-            (followedCommunity) => followedCommunity.community.id
-          ),
+    const followedCommunities = await db.subscription.findMany({
+        where: {
+            userId: session?.user?.id,
         },
-      },
-    },
-    
-    orderBy: {
-      createdAt: "desc",
-    },
-    include: {
-      votes: true,
-      author: true,
-      comments: true,
-      community: true,
-    },
-    take: INFINITE_SCROLLING_PAGINATION_RESULTS,
-  });
+        include: {
+            community: true,
+        },
+    });
 
-  return <PostFeed initialPosts={posts} />;
+    const posts = await db.post.findMany({
+        where: {
+            community: {
+                name: {
+                    in: followedCommunities.map(
+                        (followedCommunity) => followedCommunity.community.id
+                    ),
+                },
+            },
+        },
+
+        orderBy: {
+            createdAt: "desc",
+        },
+        include: {
+            votes: true,
+            author: true,
+            comments: true,
+            community: true,
+            bookmarks: true,
+        },
+        take: INFINITE_SCROLLING_PAGINATION_RESULTS,
+    });
+
+    return <PostFeed initialPosts={posts} />;
 };
 
 export default CustomFeed;
