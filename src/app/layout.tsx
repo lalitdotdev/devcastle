ec34bd50045fecdba25c395d8e087833191a50cd
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-29 20:09:11 +0530

    feat(sonner): 💄 Add Sonner Toaster component with theme support and custom styling for improved toast notifications.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import "@/styles/globals.css";
 
-import { Inter, Poppins } from "next/font/google";
-
 import Navbar from "@/components/Navbar";
+import NextTopLoader from 'nextjs-toploader';
+import { Poppins } from "next/font/google";
 import Providers from "@/components/Providers";
 import Sidebar from "@/components/sidebar/sidebar";
+import { SonnerToaster } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
+import clsx from "clsx";
 import { cn } from "@/lib/utils";
+import { getAuthSession } from "@/lib/auth";
 
 export const metadata = {
     title: "DevCastle",
@@ -18,13 +21,17 @@ export const metadata = {
 
 const inter = Poppins({ subsets: ["latin"], weight: ['100', '200', '300', '400', '500', '600', '700'] });
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
     authModal, // receiving authModal as a prop as it is a server component
 }: {
     children: React.ReactNode;
     authModal?: React.ReactNode;
 }) {
+
+    const session = await getAuthSession();
+
+
     return (
         <html
             lang="en"
@@ -33,24 +40,39 @@ export default function RootLayout({
                 inter.className
             )}
         >
-            <body className="min-h-screen pt-12 bg-[#1B1F23] antialiased">
+            <body className="min-h-screen pt-12 bg-[#1B1F23] antialiased pb-4">
                 <Providers>
+                    <NextTopLoader />
                     <header className="h-20 fixed inset-y-0 w-full z-50">
                         {/* @ts-expect-error Server Component */}
                         <Navbar />
                     </header>
 
 
-                    <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+
+                    {/* Sidebar only for auth user */}
+                    {/* {session?.user && ( */}
+                    <div className="fixed inset-y-0 left-0 z-50 w-64">
                         <Sidebar />
                     </div>
+                    {/* )} */}
+
+
+
+
+
                     {/* Rendering authModal alongside other components */}
                     {authModal}
-                    <div className="container max-w-7xl mx-auto h-full pt-12">
+
+                    <div className="container max-w-9xl mx-auto h-full pt-12">
                         {children}
                     </div>
+
+
+
                 </Providers>
                 <Toaster />
+                <SonnerToaster />
             </body>
         </html>
     );
---
+++ src/components/ui/sonner.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { Toaster as Sonner } from "sonner"
+import { useTheme } from "next-themes"
+
+type ToasterProps = React.ComponentProps<typeof Sonner>
+
+const SonnerToaster = ({ ...props }: ToasterProps) => {
+    const { theme = "system" } = useTheme()
+
+    return (
+        <Sonner
+            theme={theme as ToasterProps["theme"]}
+            className="toaster group"
+            toastOptions={{
+                classNames: {
+                    toast:
+                        "group toast group-[.toaster]:bg-[#212329] group-[.toaster]:text-blue-600 group-[.toaster]:border-slate-700 group-[.toaster]:shadow-lg group-[.toaster]:backdrop-blur-2xl group-[.toaster]:rounded-xl group-[.toaster]:text-indigo-600",
+                    description: "group-[.toast]:text-blue-600",
+                    actionButton:
+                        "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+                    cancelButton:
+                        "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+                },
+            }}
+            {...props}
+        />
+    )
+}
+
+export { SonnerToaster }
