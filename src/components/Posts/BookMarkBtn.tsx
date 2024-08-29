6d8a28df14edd61a736dc14d8608f544ab0d8e32
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-30 01:17:41 +0530

    🔖 Add bookmark toggle functionality with visual feedback with sonner taost  and error handling

--- src/components/Posts/BookMarkBtn.tsx
+++ src/components/Posts/BookMarkBtn.tsx
@@ -42,7 +42,6 @@ export default function BookmarkButton({
         onMutate: async () => {
             toast.success(`Post ${data.isBookmarkedByUser ? "un" : ""}bookmarked`);
             await queryClient.cancelQueries({ queryKey });
-
             const previousState = queryClient.getQueryData<BookmarkInfo>(queryKey);
 
             queryClient.setQueryData<BookmarkInfo>(queryKey, () => ({
