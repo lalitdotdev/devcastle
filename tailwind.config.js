2e1fe41e6b85a6a9f7ebbcdbbc562ce9f290283e
Lalit sharma <geekymonkcp742k@gmail.com>
2024-09-13 13:56:07 +0530

    add changes to container width and font family.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import { fontFamily } from "tailwindcss/defaultTheme";
+
 const plugin = require("tailwindcss/plugin");
 
 /** @type {import('tailwindcss').Config} */
@@ -10,158 +12,164 @@ module.exports = {
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
-  	container: {
-  		center: 'true',
-  		padding: '1rem',
-  		screens: {
-  			'2xl': '1400px'
-  		}
-  	},
-  	extend: {
-  		backgroundImage: {
-  			'rec-pattern': 'url(/assets/images/recCommsArt.svg)',
-  			'footer-texture': 'url(/img/footer-texture.png)',
-  			'castle-art': 'url(/assets/images/castle-art.jpg)',
-  			'conic-gradient': 'conic-gradient(from 225deg, #FFC876, #79FFF7, #9F53FF, #FF98E2, #FFC876)'
-  		},
-  		colors: {
-  			transparent: 'transparent',
-  			'dark-charcoal': '#101214',
-  			'super-dark-gray': '#1A1A1A',
-  			'dim-gray': '#555555',
-  			'light-gray': '#878787',
-  			'primary-gray': '#1B1B1B',
-  			darkBlue: '#217093',
-  			medBlue: '#4eb8dd',
-  			lightBlue: '#ddf1fa',
-  			inputBG: '#f3fafd',
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			text: '#CCCCCC',
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			outline: '#D1D1D1',
-  			gradient: '#D0C7B3',
-  			gradient2: '#968A73',
-  			gradient3: '#675E4C',
-  			color: {
-  				'1': '#AC6AFF',
-  				'2': '#FFC876',
-  				'3': '#FF776F',
-  				'4': '#7ADB78',
-  				'5': '#858DFF',
-  				'6': '#FF98E2',
-  				dark: '#1b1b1b',
-  				light: '#fff',
-  				accent: '#7B00D3',
-  				accentDark: '#ffdb4d',
-  				accentBlue: '#4B83F0',
-  				gray: '#747474'
-  			},
-  			stroke: {
-  				'1': '#26242C'
-  			},
-  			n: {
-  				'1': '#FFFFFF',
-  				'2': '#CAC6DD',
-  				'3': '#ADA8C3',
-  				'4': '#757185',
-  				'5': '#3F3A52',
-  				'6': '#252134',
-  				'7': '#15131D',
-  				'8': '#0E0C15',
-  				'9': '#474060',
-  				'10': '#43435C',
-  				'11': '#1B1B2E',
-  				'12': '#2E2A41',
-  				'13': '#6C7275',
-  				'14': '#444746'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		keyframes: {
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			},
-  			gradient: {
-  				'0%, 100%': {
-  					backgroundPosition: '0% 50%'
-  				},
-  				'100%': {
-  					backgroundPosition: '100% 50%'
-  				}
-  			},
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
-  		},
-  		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out',
-  			gradient: 'gradient 6s linear infinite',
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
-  		}
-  	}
+    container: {
+      center: "true",
+      padding: "1rem",
+      screens: {
+        "2xl": "1500px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        heading: ["var(--font-heading)", ...fontFamily.sans],
+        body: ["var(--font-body)", ...fontFamily.sans],
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+      backgroundImage: {
+        "rec-pattern": "url(/assets/images/recCommsArt.svg)",
+        "footer-texture": "url(/img/footer-texture.png)",
+        "castle-art": "url(/assets/images/castle-art.jpg)",
+        "conic-gradient":
+          "conic-gradient(from 225deg, #FFC876, #79FFF7, #9F53FF, #FF98E2, #FFC876)",
+      },
+      colors: {
+        transparent: "transparent",
+        "dark-charcoal": "#101214",
+        "super-dark-gray": "#1A1A1A",
+        "dim-gray": "#555555",
+        "light-gray": "#878787",
+        "primary-gray": "#1B1B1B",
+        darkBlue: "#217093",
+        medBlue: "#4eb8dd",
+        lightBlue: "#ddf1fa",
+        inputBG: "#f3fafd",
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        text: "#CCCCCC",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        outline: "#D1D1D1",
+        gradient: "#D0C7B3",
+        gradient2: "#968A73",
+        gradient3: "#675E4C",
+        color: {
+          1: "#AC6AFF",
+          2: "#FFC876",
+          3: "#FF776F",
+          4: "#7ADB78",
+          5: "#858DFF",
+          6: "#FF98E2",
+          dark: "#1b1b1b",
+          light: "#fff",
+          accent: "#7B00D3",
+          accentDark: "#ffdb4d",
+          accentBlue: "#4B83F0",
+          gray: "#747474",
+        },
+        stroke: {
+          1: "#26242C",
+        },
+        n: {
+          1: "#FFFFFF",
+          2: "#CAC6DD",
+          3: "#ADA8C3",
+          4: "#757185",
+          5: "#3F3A52",
+          6: "#252134",
+          7: "#15131D",
+          8: "#0E0C15",
+          9: "#474060",
+          10: "#43435C",
+          11: "#1B1B2E",
+          12: "#2E2A41",
+          13: "#6C7275",
+          14: "#444746",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+        gradient: {
+          "0%, 100%": {
+            backgroundPosition: "0% 50%",
+          },
+          "100%": {
+            backgroundPosition: "100% 50%",
+          },
+        },
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        gradient: "gradient 6s linear infinite",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
   plugins: [
     plugin(function ({ addBase, addComponents, addUtilities }) {
