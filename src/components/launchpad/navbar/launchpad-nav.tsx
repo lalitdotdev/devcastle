d8af57f6ad4ebc32f4feea0ad9a394e3b5266cc3
lalit sharma <geekymonkcp742k@gmail.com>
2024-09-14 22:58:04 +0530

    feat(LaunchPadNavbar): Add LaunchPadNavbar component with search, menu, and logo functionality

---
+++ src/components/launchpad/navbar/launchpad-nav.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Logo from "./logo";
+import Menu from "./menu";
+import Search from "./search";
+import { Separator } from "@/components/ui/separator";
+
+interface NavbarProps {
+    authenticatedUser?: any;
+    notifications?: any;
+    products?: any;
+}
+
+const LaunchPadNavbar: React.FC<NavbarProps> = ({
+    authenticatedUser,
+    notifications,
+    products,
+}) => {
+
+
+    return (
+        <div className="py-2 md:py-0 px-4 md:px-6  rounded-b-3xl w-full backdrop-filter backdrop-blur-3xl border-slate-600 my-8">
+            <div className="flex relative items-center justify-between  bg-teal-900">
+                <div className="flex items-center z-[999] absolute left-0">
+                    <Search />
+                </div>
+                <div className="absolute right-0 transform z-10 ">
+                    <Menu />
+                </div>
+
+            </div>
+            <Separator className="bg-zinc-600 my-8 rounded-lg" />
+
+        </div>
+    );
+};
+
+export default LaunchPadNavbar;
---
+++ src/components/launchpad/navbar/menu.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import AboutMenu from "./menus/about-menu";
+import CommunityMenu from "./menus/community-menu";
+import LaunchesMenu from "./menus/launches-menu";
+import Link from "next/link";
+import { useState } from "react";
+
+const Menu = () => {
+    const [showLaunchesMenu, setShowLaunchesMenu] = useState(false);
+    const [showCommunityMenu, setShowCommunityMenu] = useState(false);
+    const [showAboutMenu, setShowAboutMenu] = useState(false);
+
+    return (
+        <div className="hidden lg:flex items-center relative  mt-2">
+            <div className="space-x-6 text-gray-300 text-md flex items-center cursor-pointer p-1 px-4">
+                <div
+                    onMouseEnter={() => setShowLaunchesMenu(true)}
+                    onMouseLeave={() => setShowLaunchesMenu(false)}
+                    className="hover:text-zinc-100 py-4"
+                >
+                    Launches {showLaunchesMenu && <LaunchesMenu />}
+                </div>
+
+                <Link href={"/categories"} className="hover:text-zinc-100">
+                    Categories
+                </Link>
+
+                <div
+                    onMouseEnter={() => setShowCommunityMenu(true)}
+                    onMouseLeave={() => setShowCommunityMenu(false)}
+                    className="hover:text-zinc-100 py-4"
+
+                >
+                    Community {showCommunityMenu && <CommunityMenu />}
+                </div>
+
+                <div className="hover:text-zinc-100 py-4">Advertise</div>
+
+                <div
+                    onMouseEnter={() => setShowAboutMenu(true)}
+                    onMouseLeave={() => setShowAboutMenu(false)}
+                    className="hover:text-zinc-100 py-4"
+
+                >
+                    About {showAboutMenu && <AboutMenu />}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Menu;
