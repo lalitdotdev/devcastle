9a22c274ec127c3f1301e09af2d792366207688f
lalit sharma <geekymonkcp742k@gmail.com>
2024-09-18 19:13:40 +0530

    feat(uploadthing sdk): 🔄 Integrated Uploadthing file upload utilities by adding generateUploadButton, generateUploadDropzone, and uploadFiles using OurFileRouter for seamless file handling.

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,5 +1,11 @@
-import { generateReactHelpers } from "@uploadthing/react/hooks";
+import {
+  generateUploadButton,
+  generateUploadDropzone,
+} from "@uploadthing/react";
 
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
+import { generateReactHelpers } from "@uploadthing/react";
 
 export const { uploadFiles } = generateReactHelpers<OurFileRouter>();
+export const UploadButton = generateUploadButton<OurFileRouter>();
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
