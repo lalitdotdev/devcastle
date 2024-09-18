eb712add56baeab3725bbc696481ffe9f1498c80
lalit sharma <geekymonkcp742k@gmail.com>
2024-09-18 19:15:19 +0530

    chore(uploadthing sdk upgradation): ✨ Updated ourFileRouter to include new routes productLogo and productImages, added error handling with UploadThingError for unauthorized uploads, and adjusted middleware structure for better request handling.

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,4 +1,5 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
 
@@ -7,16 +8,28 @@ const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "4MB" } })
-    .middleware(async (req) => {
+    .middleware(async ({ req }) => {
       const user = await auth(req);
 
-      if (!user) throw new Error("Unauthorized");
+      // If you throw, the user will not be able to upload
+      if (!user) throw new UploadThingError("Unauthorized");
 
       return { userId: user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
     }),
+  productLogo: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    .middleware(async ({ req }) => {
+      return { ...req };
+    })
+    .onUploadComplete(() => {}),
+
+  productImages: f({ image: { maxFileSize: "4MB", maxFileCount: 5 } })
+    .middleware(async ({ req }) => {
+      return { ...req };
+    })
+    .onUploadComplete(() => {}),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
