9cec0efd5c4d4855d388296480f58b908e1b7722
lalit sharma <geekymonkcp742k@gmail.com>
2024-09-23 11:41:14 +0530

    added shortcuts on rightNav content

--- src/components/RightNavContent.tsx
+++ src/components/RightNavContent.tsx
@@ -6,7 +6,8 @@ import {
     DropdownMenuContent,
     DropdownMenuItem,
     DropdownMenuSeparator,
-    DropdownMenuTrigger,
+    DropdownMenuShortcut,
+    DropdownMenuTrigger
 } from "./ui/DropdownMenu";
 
 import Link from "next/link";
@@ -16,76 +17,53 @@ const RightNavContent = () => {
     return (
         <div className="flex-shrink-0 md:block md:flex-shrink-0 md:items-center gap-4">
             <div className="text-gray-400 mr-4">
-                <DropdownMenu >
+                <DropdownMenu>
                     <DropdownMenuTrigger asChild>
-                        <div className="flex items-center justify-center  p-1 md:p-2 rounded-md transition-all duration-300 ease-in-out gap-2  border border-zinc-700 cursor-pointer mt-1 md:mt-2 ">
+                        <div className="relative flex items-center justify-center p-1 md:p-2 rounded-md transition-transform duration-300 ease-in-out gap-2 border border-zinc-700 cursor-pointer mt-1 md:mt-2 hover:bg-gray-800 hover:border-gray-600 transform hover:scale-105">
                             <Terminal size={20} className="text-gray-500" />
                             <span>
                                 <Plus size={18} className="text-gray-500" />
                             </span>
                         </div>
                     </DropdownMenuTrigger>
-                    <DropdownMenuContent className="w-40 bg-[#2D333B] text-gray-400 border border-gray-600 rounded-2xl items-center justify-center p-2 transition-all duration-300 ease-in-out">
-
-                        <DropdownMenuItem className="cursor-pointer " asChild>
-                            <Link
-                                href="/cb/create"
-                                className={cn(
-                                    "gap-2"
-                                )}
-                            >
-                                <Plus size={20} />
+                    <DropdownMenuContent className="w-56 bg-gray-800 text-gray-400 border border-gray-600 rounded-xl p-2 transition-transform duration-300 ease-in-out shadow-lg">
+                        <DropdownMenuItem className="flex items-center gap-2 p-2 rounded-md transition-colors duration-300 hover:bg-gray-700 hover:text-gray-100" asChild>
+                            <Link href="/cb/create" className={cn("flex items-center gap-2")}>
+                                <Plus size={20} className="text-gray-300" />
                                 <span>New Castle</span>
+                                <DropdownMenuShortcut>Ctrl+N</DropdownMenuShortcut>
                             </Link>
                         </DropdownMenuItem>
-                        <DropdownMenuSeparator className="bg-gray-700 " />
-                        <DropdownMenuItem className="cursor-pointer " asChild>
-                            <Link
-                                href="/courses/new"
-                                className={cn(
-                                    "gap-2"
-                                )}
-                            >
-                                <BrainCog size={22} />
+                        <DropdownMenuSeparator className="bg-gray-700" />
+                        <DropdownMenuItem className="flex items-center gap-2 p-2 rounded-md transition-colors duration-300 hover:bg-gray-700 hover:text-gray-100" asChild>
+                            <Link href="/courses/new" className={cn("flex items-center gap-2")}>
+                                <BrainCog size={22} className="text-gray-300" />
                                 <span>New thought?</span>
+                                <DropdownMenuShortcut>Ctrl+T</DropdownMenuShortcut>
                             </Link>
                         </DropdownMenuItem>
-                        <DropdownMenuItem className="cursor-pointer " asChild>
-                            <Link
-                                href="/articles/publish"
-                                className={cn(
-                                    " flex p-2 rounded-md transition-all duration-300 ease-in-out gap-2"
-                                )}
-                            >
-                                <Feather size={22} />
+                        <DropdownMenuItem className="flex items-center gap-2 p-2 rounded-md transition-colors duration-300 hover:bg-gray-700 hover:text-gray-100" asChild>
+                            <Link href="/articles/publish" className={cn("flex items-center gap-2")}>
+                                <Feather size={22} className="text-gray-300" />
                                 New Article
+                                <DropdownMenuShortcut>Ctrl+E</DropdownMenuShortcut>
                             </Link>
                         </DropdownMenuItem>
-                        <DropdownMenuSeparator className="bg-gray-700 " />
-                        <DropdownMenuItem className="cursor-pointer " asChild>
-                            <Link
-                                href="/projects/new"
-                                className={cn(
-                                    "gap-2"
-                                )}
-                            >
-                                <TerminalSquare size={22} />
+                        <DropdownMenuSeparator className="bg-gray-700" />
+                        <DropdownMenuItem className="flex items-center gap-2 p-2 rounded-md transition-colors duration-300 hover:bg-gray-700 hover:text-gray-100" asChild>
+                            <Link href="/projects/new" className={cn("flex items-center gap-2")}>
+                                <TerminalSquare size={22} className="text-gray-300" />
                                 <span>New Project</span>
+                                <DropdownMenuShortcut>Ctrl+P</DropdownMenuShortcut>
                             </Link>
                         </DropdownMenuItem>
-                        <DropdownMenuItem className="cursor-pointer " asChild>
-                            <Link
-                                href="/jobs/new"
-                                className={cn(
-                                    " flex p-2 rounded-md transition-all duration-300 ease-in-out gap-2"
-                                )}
-                            >
-                                <Backpack size={22} />
+                        <DropdownMenuItem className="flex items-center gap-2 p-2 rounded-md transition-colors duration-300 hover:bg-gray-700 hover:text-gray-100" asChild>
+                            <Link href="/jobs/new" className={cn("flex items-center gap-2")}>
+                                <Backpack size={22} className="text-gray-300" />
                                 Post a Job
+                                <DropdownMenuShortcut>Ctrl+J</DropdownMenuShortcut>
                             </Link>
                         </DropdownMenuItem>
-
-
                     </DropdownMenuContent>
                 </DropdownMenu>
             </div>
