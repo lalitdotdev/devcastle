9a10582031e0098196c49885271afc3dee6969f8
lalit sharma <geekymonkcp742k@gmail.com>
2024-09-29 17:32:20 +0530

    fix(navbar): improve layout and alignment for small screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,8 @@
-import RabbitIcon, { Icons } from "./Icons";
-
 import { Badge } from "./ui/badge";
 import KeyboardShortcuts from "@/lib/hotkeys";
 import Link from "next/link";
 import NotificationIcon from "./Navbar/NotificationIcon";
+import RabbitIcon from "./Icons";
 import RightNavContent from "./RightNavContent";
 import SearchBar from "./SearchBar";
 import { Separator } from "./ui/separator";
@@ -17,85 +16,75 @@ const Navbar = async () => {
     const session = await getServerSession(authOptions);
 
     return (
-        <nav className="m-auto fixed h-fit top-0  -z-[10] py-2 md:py-4 align-center justify-center  bg-[#1B1F23]  left-0 w-full border-b-2 border-zinc-800 backdrop-filter backdrop-blur-2xl">
-            <div className="container max-w-7xl justify-between min-w-max mx-auto flex items-center gap-2 px-5 md:px-4 py-2 ">
+        <nav className="fixed top-0 w-full z-10 bg-[#1B1F23] border-b-2 border-zinc-800 backdrop-blur-lg">
+            <div className="container max-w-7xl mx-auto flex justify-between items-center py-3 px-4 md:px-6">
 
-                <Link
-                    href="/"
-                    className="items-center justify-center gap-2 sm:flex text-zinc-400"
-                >
+                {/* Logo */}
+                <Link href="/" className="flex items-center gap-2 text-zinc-400">
                     <TerminalSquare
-                        size={42}
-                        className="animate-pulse transition-all duration-800 ease-in-out text-indigo-600"
+                        size={30} // Smaller size for small screens
+                        className="text-indigo-600"
                     />
-
-                    <span className="font-md hidden md:block items-center justify-center text-xl">
-                        devcastle
-                    </span>
-
-
+                    <span className="hidden md:block text-xl">devcastle</span> {/* Only show on medium screens and above */}
                 </Link>
 
-                {/* Search-Bar */}
-                <div className="flex justify-center items-center gap-3">
-                    <SearchBar />
+                {/* Search Bar and Nav Links */}
+                <div className="flex items-center">
+                    {/* Search Bar */}
+                    <div className=" items-center mx-1">
+                        <SearchBar />
+                    </div>
 
-                    <Separator orientation="vertical" className="h-6 w-0.5 bg-zinc-600" />
-                    {/* <NavbarRoutes /> */}
-                    <div className="flex">
-                        {session?.user?.email && (
-                            <>
-                                <RightNavContent />
-                                <KeyboardShortcuts />
+                    {/* Separator for medium screens */}
+                    <Separator orientation="vertical" className="h-6 w-0.5 bg-zinc-600 hidden sm:block mx-1" />
 
-                            </>
-                        )}
-
-                        {/* TODO: THEMETOGGLE BUTTON HERE */}
-
-                        {/* actions */}
-                        <div className="flex items-center">
-                            {session?.user?.email && (
-                                <>
-                                    <div className="relative mr-2 border rounded-xl border-zinc-800 py-1 flex items-center justify-center">
-                                        <Link
-                                            href="/launchpad"
-                                            className={buttonVariants({
-                                                variant: "ghost",
-                                            })}
-                                        >
-                                            <RabbitIcon className="text-teal-300" />
-                                        </Link>
-                                        <Badge
-                                            variant="outline"
-                                            className="absolute -top-2 -right-2 px-1 py-0.5 text-[0.6rem] bg-teal-300 text-yellow-800 border-yellow-300"
-                                        >
-                                            WIP
-                                        </Badge>
-                                    </div>
-                                    <NotificationIcon />
-                                </>
-                            )}
-
-                            {/* TODO: THEMETOGGLE BUTTON HERE */}
-
-                            {/* actions */}
-                            {session?.user ? (
-                                <UserAccountNav user={session.user} username={session?.user?.username} />
-                            ) : (
-                                <Link href="/sign-in" className={buttonVariants({
-                                    size: "sm",
-                                    className: "text-zinc-300 border border-zinc-500 "
-                                })}>
-                                    SignIn
-                                </Link>
-                            )}
+                    {/* Right Side Content */}
+                    {session?.user?.email && (
+                        <div className="hidden md:flex items-center mx-1">
+                            <RightNavContent />
+                            <KeyboardShortcuts />
                         </div>
+                    )}
+
+                    <Separator orientation="vertical" className="h-6 w-0.5 bg-zinc-600 hidden sm:block mx-1" />
+
+                    {/* Rabbit Icon with Badge */}
+                    {session?.user?.email && (
+                        <div className="relative flex items-center mr-3 border rounded-xl border-zinc-800 py-2 px-2">
+                            <Link href="/launchpad" className="text-teal-300">
+                                <RabbitIcon className="text-teal-500" />
+                            </Link>
+                            <Badge
+                                variant="outline"
+                                className="absolute -top-2 -right-2 px-1 py-0.5 text-[0.6rem] bg-teal-300 text-yellow-800 border-yellow-300"
+                            >
+                                WIP
+                            </Badge>
+                        </div>
+                    )}
+
+                    {/* Notification Icon */}
+                    {session?.user?.email && (
+                        <NotificationIcon />
+                    )}
+
+
+                    {/* User Account or Sign-in */}
+                    <div className="flex items-center gap-4">
+                        {session?.user ? (
+                            <UserAccountNav user={session.user} username={session?.user?.username} />
+                        ) : (
+                            <Link href="/sign-in" className={buttonVariants({
+                                size: "sm",
+                                className: "text-zinc-300 border border-zinc-500 "
+                            })}>
+                                Sign In
+                            </Link>
+                        )}
                     </div>
                 </div>
             </div>
-
-        </nav >
+        </nav>
     );
 };
 
