dfb4bac6c53da5ba392f7c980374dd009eac3650
lalit sharma <geekymonkcp742k@gmail.com>
2024-10-03 22:51:29 +0530

    (Markdown Job board details): Added Markdown component with custom link renderer and styled list support using ReactMarkdown, rehypeRaw, and remarkGfm.

--- src/components/Jobboard/Markdown.tsx
+++ src/components/Jobboard/Markdown.tsx
@@ -1,18 +1,25 @@
+import CustomLinkRenderer from "../renderers/CustomLinkRenderer";
 import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
+import remarkGfm from "remark-gfm";
 
 interface MarkdownProps {
     children: string;
 }
 
+
 export default function Markdown({ children }: MarkdownProps) {
     return (
         <ReactMarkdown
+            rehypePlugins={[rehypeRaw]}
+            remarkPlugins={[remarkGfm]}
             className="space-y-3 p-2  md:p-8 border-2 border-gray-600 rounded-md text-neutral-300"
+
             components={{
                 ul: (props) => <ul className="list-inside list-disc marker:text-blue-600 marker:text-lg " {...props} />,
-                a: (props) => (
-                    <a className="text-green-500 underline" target="_blank" {...props} />
-                ),
+                a: ({ href, children }) => <CustomLinkRenderer href={href || ''}>{children}</CustomLinkRenderer>,
+
+
 
             }}
         >
