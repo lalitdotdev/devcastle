056858f006794d2c1d586bbb011df3d388c68846
lalit sharma <geekymonkcp742k@gmail.com>
2024-10-11 14:33:06 +0530

    refactor(rightnav): Add upgrade modal for premium users in product creation

--- src/components/RightNavContent.tsx
+++ src/components/RightNavContent.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Backpack, BrainCog, Feather, Plus, Rocket, Terminal, TerminalSquare } from "lucide-react";
+import { Backpack, BrainCog, Feather, Plus, Rocket, Terminal } from "lucide-react";
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -11,18 +11,42 @@ import {
 } from "./ui/DropdownMenu";
 
 import Link from "next/link";
+import MembershipModal from "./modals/upgrade-membership-modal";
+import UpgradeMembership from "./launchpad/upgrade-membership";
 import { cn } from "@/lib/utils";
+import { isUserPremium } from "@/lib/launchpad-server-actions/server-actions";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-const RightNavContent = () => {
+interface RightNavContentProps {
+    products: any;
+    authenticatedUser: any;
+}
+
+const RightNavContent = ({ products, authenticatedUser }: RightNavContentProps) => {
+
+    const router = useRouter();
+
+    const [isUpgradeModalVisible, setIsUpgradeModalVisible] = useState(false);
+
+    const handleClick = async () => {
+        const isPremium = await isUserPremium()
+        if (!isPremium && products.length === 2) {
+            setIsUpgradeModalVisible(true);
+        } else {
+            router.push("/launchpad/new-product");
+        }
+    };
+
     return (
-        <div className="flex-shrink-0 md:block md:flex-shrink-0 md:items-center gap-4">
+        <div className="flex-shrink-0 md:items-center gap-4">
             <div className="text-gray-400 ">
                 <DropdownMenu>
                     <DropdownMenuTrigger asChild>
-                        <div className="relative flex items-center justify-center p-1 md:p-2 rounded-md transition-transform duration-300 ease-in-out gap-2 border border-zinc-700 cursor-pointer mt-1 md:mt-2 hover:bg-gray-800 hover:border-gray-600 transform hover:scale-105">
+                        <div className="relative flex items-center justify-center  rounded-md transition-transform duration-300 ease-in-out gap-2 border border-zinc-700 cursor-pointer p-1 hover:bg-gray-800 hover:border-gray-600 transform hover:scale-105">
                             <Terminal size={20} className="text-gray-500" />
                             <span>
-                                <Plus size={18} className="text-gray-500" />
+                                <Plus size={18} className="text-gray-500 " />
                             </span>
                         </div>
                     </DropdownMenuTrigger>
@@ -51,11 +75,18 @@ const RightNavContent = () => {
                         </DropdownMenuItem>
                         <DropdownMenuSeparator className="bg-gray-700" />
                         <DropdownMenuItem className="flex items-center gap-2 p-2 rounded-md transition-colors duration-300 hover:bg-gray-700 hover:text-gray-100" asChild>
-                            <Link href="/launchpad/new-product" className={cn("flex items-center gap-2")}>
+                            <button onClick={handleClick} className={cn("flex items-center gap-2 w-full")}>
                                 <Rocket size={22} className="text-lime-500" />
                                 <span>New Product</span>
                                 <DropdownMenuShortcut>Ctrl+P</DropdownMenuShortcut>
-                            </Link>
+
+                                <MembershipModal
+                                    visible={isUpgradeModalVisible}
+                                    setVisible={setIsUpgradeModalVisible}
+                                >
+                                    <UpgradeMembership authenticatedUser={authenticatedUser} />
+                                </MembershipModal>
+                            </button>
                         </DropdownMenuItem>
                         <DropdownMenuItem className="flex items-center gap-2 p-2 rounded-md transition-colors duration-300 hover:bg-gray-700 hover:text-gray-100" asChild>
                             <Link href="/jobs/new" className={cn("flex items-center gap-2")}>
