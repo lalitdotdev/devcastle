6fc43689b32754939b891f4993fe7af86878fac2
lalit sharma <geekymonkcp742k@gmail.com>
2024-10-11 12:05:15 +0530

    Add allowed external domains (techcrunch.com, img-cdn.publive.online) and configure webpack to handle .json files in Next.js config.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: "buildNext",
   images: {
     remotePatterns: [
       {
@@ -36,7 +37,22 @@ const nextConfig = {
         protocol: "https",
         hostname: "thestartupmag.com",
       },
+      {
+        protocol: "https",
+        hostname: "techcrunch.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img-cdn.publive.online",
+      },
     ],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.json$/,
+      type: "json",
+    });
+    return config;
   },
   eslint: {
     ignoreDuringBuilds: true,
