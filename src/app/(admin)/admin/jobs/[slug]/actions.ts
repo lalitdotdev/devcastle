86c19e2c684415a776ee227400fd705e90ded056
lalit sharma <geekymonkcp742k@gmail.com>
2024-10-21 01:24:29 +0530

    fix(jobs server actions): 🐛 fix: parse jobId to integer when handling formData in approveSubmission and deleteJob.

--- src/app/(admin)/admin/jobs/[slug]/actions.ts
+++ src/app/(admin)/admin/jobs/[slug]/actions.ts
@@ -1,18 +1,19 @@
 "use server";
-import { getAuthSession } from "@/lib/auth";
+
 import { db } from "@/lib/db";
 import { del } from "@vercel/blob";
+import { getAuthSession } from "@/lib/auth";
 import { revalidatePath } from "next/cache";
 
 type FormState = { error?: string } | undefined;
 
 export async function approveSubmission(
   prevState: FormState,
-  formData: FormData
+  formData: FormData,
 ): Promise<FormState> {
   try {
     // throw new Error("Not implemented");
-    const jobId = parseInt(formData.get("jobId") as string);
+    const jobId = formData.get("jobId") as string;
     const session = await getAuthSession();
     if (session?.user.role !== "ADMIN") {
       throw new Error("Not Authorized !!");
@@ -35,11 +36,11 @@ export async function approveSubmission(
 // delete a job server action
 export async function deleteJob(
   prevState: FormState,
-  formData: FormData
+  formData: FormData,
 ): Promise<FormState> {
   try {
     // throw new Error("Not implemented");
-    const jobId = parseInt(formData.get("jobId") as string);
+    const jobId = formData.get("jobId") as string;
     const session = await getAuthSession();
     if (session?.user.role !== "ADMIN") {
       throw new Error("Not Authorized !!");
