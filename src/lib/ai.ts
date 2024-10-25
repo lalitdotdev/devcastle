6666e0a967902587b3a54e30a347c25930067948
lalit sharma <geekymonkcp742k@gmail.com>
2024-10-25 14:49:45 +0530

    ✨ feat(ai): Integrate Google Generative AI - implement generative model configuration using 'gemini-1.5-flash' 🚀.

---
+++ src/lib/ai.ts
@@ -0,0 +1,60 @@
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} = require("@google/generative-ai");
+
+const apiKey = process.env.GEMINI_API_KEY;
+
+// Add this check to ensure code only runs on server
+if (!process.env.GEMINI_API_KEY) {
+  throw new Error("GEMINI_API_KEY is not defined in environment variables");
+}
+
+console.log("API Key:", apiKey);
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function getGenerativeAIResponse(prompt: string) {
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ];
+
+  const chatSession = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+  return result.response.text().trim().replace(/```/g, "");
+}
+
+export default getGenerativeAIResponse;
