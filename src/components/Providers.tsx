727505b315cf72593221b24b09e5f3de47cee423
lalit sharma <geekymonkcp742k@gmail.com>
2024-10-31 14:16:25 +0530

    feat(ui): add tooltip and provider in root layout.

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,33 +1,35 @@
 "use client";
 
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 import { FC, ReactNode } from "react";
+import { TooltipProvider } from "./ui/tooltip";
 
 interface LayoutProps {
-  children: ReactNode;
+    children: ReactNode;
 }
 
 const queryClient = new QueryClient();
 
 const Providers: FC<LayoutProps> = ({
-  children,
-  ...props
+    children,
+    ...props
 }: ThemeProviderProps) => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      {/* <NextThemesProvider
+    return (
+        <QueryClientProvider client={queryClient}>
+            {/* <NextThemesProvider
         attribute="class"
         defaultTheme="light"
         enableSystem
         {...props}
       > */}
-      <SessionProvider>{children}</SessionProvider>
-      {/* </NextThemesProvider> */}
-    </QueryClientProvider>
-  );
+            <TooltipProvider>
+                <SessionProvider>{children}</SessionProvider>
+            </TooltipProvider>
+            {/* </NextThemesProvider> */}
+        </QueryClientProvider>
+    );
 };
 
 export default Providers;
